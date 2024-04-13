c41fd9350f69831e68cb58827a7ecbf07a2bfcf6
Bhatti0789 <muhammadabdullahbinbabar@gmail.com>
2024-04-13 14:25:07 +0500

    feat✨()：

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+// Creating a Cli To Do List
+import inquirer from "inquirer";
+let toDo = [];
+let condition = true;
+while (condition) {
+    let input = await inquirer.prompt([
+        {
+            name: "task",
+            message: "To Do List",
+            type: "input"
+        },
+        {
+            name: "tasks",
+            message: "Do you want to Add More?",
+            type: "confirm"
+        }
+    ]);
+    toDo.push(input.task);
+    condition = input.tasks;
+}
+console.log(toDo);
